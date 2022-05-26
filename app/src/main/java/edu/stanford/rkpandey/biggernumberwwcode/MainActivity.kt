f25c9f7e1a5950ee0c3c2f7c54fba4d501207f16
beibarys-jedi <bumirbayev@jmart.kz>
2022-05-26 11:12:52 +0600

    first commit

--- app/src/main/java/edu/stanford/rkpandey/biggernumberwwcode/MainActivity.kt
+++ app/src/main/java/edu/stanford/rkpandey/biggernumberwwcode/MainActivity.kt
@@ -2,6 +2,7 @@ package edu.stanford.rkpandey.biggernumberwwcode
 
 import android.graphics.Color
 import android.os.Bundle
+import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
@@ -13,43 +14,43 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        assignNumbersToButtons()
+        assignRandomNumbersToButtons()
         btnLeft.setOnClickListener {
-            checkAnswer(true)
-            assignNumbersToButtons()
+            compareTwoButtons(true)
+            assignRandomNumbersToButtons()
         }
 
         btnRight.setOnClickListener {
-            checkAnswer(false)
-            assignNumbersToButtons()
+            compareTwoButtons(false)
+            assignRandomNumbersToButtons()
         }
     }
 
-    private fun assignNumbersToButtons() {
+    private fun compareTwoButtons(isLeftBtnClicked: Boolean) {
+        val leftNum = btnLeft.text.toString().toInt()
+        val rightNum = btnRight.text.toString().toInt()
+
+        val isAnswerCorrect = if (isLeftBtnClicked) leftNum > rightNum else rightNum > leftNum
+        if (isAnswerCorrect) {
+            backgroundView.setBackgroundColor(Color.GREEN)
+            Toast.makeText(this, "correct", Toast.LENGTH_SHORT).show()
+        } else {
+            backgroundView.setBackgroundColor(Color.RED)
+            Toast.makeText(this, "wrong number", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun assignRandomNumbersToButtons() {
         val r = Random()
         val num1 = r.nextInt(10)
         var num2 = num1
         while (num2 == num1) {
             num2 = r.nextInt(10)
         }
+
         btnLeft.text = "$num1"
         btnRight.text = "$num2"
     }
 
-    private fun checkAnswer(isLeftButtonSelected: Boolean) {
-        val n1 = btnLeft.text.toString().toInt()
-        val n2 = btnRight.text.toString().toInt()
-        val isAnswerCorrect = if (isLeftButtonSelected) n1 > n2 else n2 > n1
-        val toastMessage: String
-        val backgroundColor: Int
-        if (isAnswerCorrect) {
-            toastMessage = "Correct!!"
-            backgroundColor = Color.GREEN
-        } else {
-            toastMessage = "Wrong"
-            backgroundColor = Color.RED
-        }
-        Toast.makeText(this, toastMessage, Toast.LENGTH_SHORT).show()
-        backgroundView.setBackgroundColor(backgroundColor)
-    }
+
 }
